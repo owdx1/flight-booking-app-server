fb1c3474057e9a26ca5bbf14898f9044878a98cd
Can Bozkurt <can.bozkurt.dhf@gmail.com>
2023-10-03 00:08:18 +0300

    cc

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(cors({
-  origin:['http://localhost:3000' , "https://biletinyo.onrender.com"]
-}));
+app.use(cors());
 app.use(express.json());
 
 
